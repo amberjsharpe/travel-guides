0dcf9086719935e78e8576da8de059a7310b48b8
Amber Sharpe <amberjsharpe@gmail.com>
2018-04-13 13:40:07 -0500

    arthur saves me

--- world-wide-travel/src/App.js
+++ world-wide-travel/src/App.js
@@ -5,31 +5,36 @@ import Background from './Background';
 import Footer from './Footer';
 import Guides from './Guides';
 import Weather from './Weather';
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header title="Travel with Fido" />
-        <footer className="footer">
-          <Footer url="https://www.gohawaii.com/" destination="Hawaii"/> 
-          <Footer url="https://tahititourisme.com/en-us/" destination="Tahiti"/>
-          <Footer url="http://www.visitjamaica.com/" destination="Jamaica"/>        
-          <Footer url="https://www.aruba.com/" destination="Aruba"/>
-        </footer>
-        <div className="body">
-          <Background />
-          <div className="weather-display">
-            <Weather />
-          </div>
-          <h2>Bork! Bork! Need a guide?</h2>
-          <div className="guide-display">
-            <Guides />
+      <Router>
+        <div className="App">
+          <Header title="Travel with Fido" />
+          <footer className="footer">
+            <Footer url="https://www.gohawaii.com/" destination="Hawaii"/> 
+            <Footer url="https://tahititourisme.com/en-us/" destination="Tahiti"/>
+            <Footer url="http://www.visitjamaica.com/" destination="Jamaica"/>        
+            <Footer url="https://www.aruba.com/" destination="Aruba"/>
+          </footer>
+          <div className="body">
+            <Background />
+            <div className="weather-display">
+              <Weather />
+            </div>
+            <h2>Bork! Bork! Need a guide?</h2>
+            <div className="guide-display">
+              <Guides />
+            </div>
           </div>
         </div>
-
-      </div>
+        </Router>
     );
   }
 }
--- world-wide-travel/src/Guides.js
+++ world-wide-travel/src/Guides.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './App.css';
 import json from './Guides.json';
 import tropical from './images/tropical.jpg'
-import { Card, CardText, CardBody,
-  CardTitle, CardImg, CardSubtitle, Button } from 'reactstrap';
+import { Card, 
+  CardText, 
+  CardBody,
+  CardTitle, 
+  CardImg, 
+  Button } from 'reactstrap';
+  import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';  
 
-function Guides(props){
-  console.log(json);
-    return (
+function Guides(){
+    return ( 
+      <Router>    
       <React.Fragment>
       {json.guides.map((j, i) => {
         return <div key={i}>
@@ -17,13 +26,14 @@ function Guides(props){
             <CardTitle>{j.title}</CardTitle>
             <CardTitle>{j.type}</CardTitle>
             <CardText>{j.price}</CardText>
-            <Button>View</Button>
+            <Button><Link to={`/${i}`}>View Guides</Link></Button>
           </CardBody>
         </Card>
       </div>     
       })}
       </React.Fragment>
-    )
+      </Router>
+    ) 
 }
 
 export default Guides
